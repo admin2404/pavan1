697e622aa1041f41309c4d7a41e2d6eccf8c872c
admin2404 <88426746+admin2404@users.noreply.github.com>
2021-08-31 12:10:37 +0530

    Update App.java

--- src/main/java/ctsproj1/App.java
+++ src/main/java/ctsproj1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello chaitanya!" );
+        System.out.println( "Hello jenkinssss!" );
     }
 }
